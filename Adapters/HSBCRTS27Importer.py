0947eeb18661cd8db76d173c8244cb914c57d412
Sarthak Agarwal <sarthak.agarwal212@gmail.com>
2018-09-09 21:52:28 +0100

    1. Use Standard Set methods to populate Table 2 and Table 6 objects 2. Set Asset Class

--- Adapters/HSBCRTS27Importer.py
+++ Adapters/HSBCRTS27Importer.py
@@ -1,21 +1,25 @@
 import csv
 import codecs
 import glob
+import os
 from datetime import datetime
+from Modules import RTS27_Prod_Class_DB_Reader_Module
 
 from Modules import RTS27_DB_Writer_Module, RTS27_Table_Records_Module
 
 hsbc_source_path = "/Users/sarthakagarwal/PycharmProjects/MifidDataAnalyser/Source/HSBC"
+rts_db_rd = RTS27_Prod_Class_DB_Reader_Module.RTS27_Prod_Class_DB_Reader()
+
 hsbcfilenames = sorted(glob.glob(hsbc_source_path+'/*.csv'))
 rtsdb = RTS27_DB_Writer_Module.RTS27_DB_Writer()
 
-PROCESS_TABLE_2="N"
-PROCESS_TABLE_4="Y"
+PROCESS_TABLE_2="Y"
+PROCESS_TABLE_4="N"
 PROCESS_TABLE_6="N"
 
 
 #filenames = filenames[0:len(filenames)
-hsbcfilenames = hsbcfilenames[0:1000]
+hsbcfilenames = hsbcfilenames[0:3]
 source_firm_name = "HSBC"
 
 print ("Array Length is" + str(len(hsbcfilenames)))
@@ -27,27 +31,33 @@ for filename in hsbcfilenames:
     # Table1 does not have much info
     with open(filename) as csvfile:
         if (("table2.csv" in str(filename)) and (PROCESS_TABLE_2=="Y")):
+
             readCSV = csv.reader(csvfile, delimiter=',')
             rowCount = 0
 
             for row in readCSV:
                 rowCount = rowCount + 1
                 if (rowCount > 1) :
+
                     # Printing output of Table 2
+                    table2_rec = RTS27_Table_Records_Module.RTS27_Table2()
+                    table2_rec.setSourceCompanyName(source_firm_name)
+                    table2_rec.setFileName(os.path.basename(filename))
                     rawdate = datetime.strptime(row[0], '%d/%m/%Y')
                     formatted_date = datetime.strftime(rawdate, "%Y-%m-%d")
-                    table2_fileIdString = source_firm_name + "_" + str(rowCount)
-                    table2_instrumentname = str(row[1]).decode('ascii', errors='ignore')
-                    table2_isin = row[2]
-                    table2_instrumentclassification = row[4]
-                    table2_currency = row[5]
-                    table2_dateofthetradingday = formatted_date
-                    table2_venue = "" # Since HSBC does not provide this Info...
+                    table2_rec.setFileId(source_firm_name + "_" + str(rowCount))
+                    table2_rec.setInstrumentName(str(row[1]).decode('ascii', errors='ignore'))
+                    table2_rec.setISIN(row[2])
+                    table2_rec.setInstrumentClassification(row[4],rts_db_rd.getCfi_assetclass_map(),rts_db_rd.getCfi_char_map())
+                    table2_rec.setCurrency(row[5])
+                    table2_rec.setTradeDate(formatted_date)
+                    table2_rec.setVenue("") # Since HSBC does not provide this Info...
 
+                    print ("isda:" , table2_rec.ISDA_ASSET_CLASS_DESC, table2_rec.INSTRUMENT_CLASSIFICATION)
+                    #print ("cfi:" , table2_rec.CFI_ATTR_1_DESC, table2_rec.CFI_ATTR_2_DESC,table2_rec.CFI_ATTR_3_DESC, table2_rec.CFI_ATTR_4_DESC, table2_rec.CFI_ATTR_5_DESC, table2_rec.CFI_ATTR_6_DESC)
                     # Writing output of Table 2
-                    # Create structure
-                    table2_rec = RTS27_Table_Records_Module.RTS27_Table2(source_firm_name, filename, table2_fileIdString, table2_isin, table2_dateofthetradingday, table2_venue, table2_instrumentname, table2_instrumentclassification, table2_currency)
-                    #rtsdb.Write_to_Table2(table2_rec)
+                    # table2_rec = RTS27_Table_Records_Module.RTS27_Table2(source_firm_name, filename, table2_fileIdString, table2_isin, table2_dateofthetradingday, table2_venue, table2_instrumentname, table2_instrumentclassification, table2_currency)
+                    rtsdb.Write_to_Table2(table2_rec)
 
         if (("table4.csv" in str(filename)) and (PROCESS_TABLE_4=="Y")):
             readCSV = csv.reader(csvfile, delimiter=',')
@@ -86,28 +96,29 @@ for filename in hsbcfilenames:
                 rowCount = rowCount + 1
                 if (rowCount > 1) :
                     # Populate Table 6 properties
+                    table6_rec = RTS27_Table_Records_Module.RTS27_Table6()
+
+
                     rawdate = datetime.strptime(row[0], '%d/%m/%Y')
                     formatted_date = datetime.strftime(rawdate, "%Y-%m-%d")
-                    table6_dateofthetradingday = formatted_date
-                    table6_fileIdString = source_firm_name + "_" + str(rowCount)
-                    table6_instrumentname = str(row[1]).decode('ascii', errors='ignore')
-                    table6_isin = row[2]
-                    table6_currency = row[3]
-                    table6_numberofordersorrequestforquotes = row[4]
-                    table6_numberoftransactionsexecuted = row[5]
-                    table6_totalvalueoftransactionsexecuted = row[6]
-                    table6_numberoftransactionscancelledorwithdrawn = row[7]
-                    table6_numberoforderorrequestmodified = row[8]
-                    table6_mediantransactionsize = row[9]
-                    table6_mediansizeofallordersorrequestsforquote = row[10]
-                    table6_numberofdesignatedmarketmakers = row[11]
+                    table6_rec.setSourceCompanyName(source_firm_name)
+                    table6_rec.setFileName(os.path.basename(filename))
+                    table6_rec.setTradeDate(formatted_date)
+                    table6_rec.setFileId(source_firm_name + "_" + str(rowCount))
+                    table6_rec.setInstrumentName(str(row[1]).decode('ascii', errors='ignore'))
+                    table6_rec.setISIN(row[2])
+                    table6_rec.setCurrency(row[3])
+                    table6_rec.setNumberOfOrderOrRequestForQuote(row[4])
+                    table6_rec.setNumberOfTransactionsExecuted(row[5])
+                    table6_rec.setTotalValueOfTransactionsExecuted(row[6])
+                    table6_rec.setNumberOfOrdersOrRequestCancelledOrWithdrawn(row[7])
+                    table6_rec.setNumberOfOrdersOrRequestModified(row[8])
+                    table6_rec.setMedianTransactionSize(row[9])
+                    table6_rec.setMedianSizeOfAllOrdersOrRequestsForQuote(row[10])
+                    table6_rec.setNumberOfDesignatedMarketMaker(row[11])
 
                     # Writing output of Table 6
                     # Create structure
-                    table6_rec = RTS27_Table_Records_Module.RTS27_Table6(source_firm_name, filename, table6_fileIdString, table6_isin, table6_dateofthetradingday, table6_instrumentname,
-                                                                         table6_numberofordersorrequestforquotes, table6_numberoftransactionsexecuted, table6_totalvalueoftransactionsexecuted,
-                                                                         table6_numberoftransactionscancelledorwithdrawn, table6_numberoforderorrequestmodified, table6_mediantransactionsize,
-                                                                         table6_mediansizeofallordersorrequestsforquote, table6_numberofdesignatedmarketmakers, table6_currency)
-                    #rtsdb.Write_to_Table6(table6_rec)
+                    rtsdb.Write_to_Table6(table6_rec)
 
 
