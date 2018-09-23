1533ccd9e82c1bf383d45211a85fb79295eee0a9
loji <loji215@gmail.com>
2018-09-23 22:22:06 +0100

    JPMorgan & UBS RTS27 importer
    Added table 1 and updated file_ID

--- Adapters/JPMorganRTS27Importer.py
+++ Adapters/JPMorganRTS27Importer.py
@@ -12,10 +12,14 @@ from datetime import datetime
 import RTS27_DB_Writer_Module
 import RTS27_Table_Records_Module
 rtsdb = RTS27_DB_Writer_Module.RTS27_DB_Writer()
+import RTS27_Prod_Class_DB_Reader_Module
+rts_db_rd = RTS27_Prod_Class_DB_Reader_Module.RTS27_Prod_Class_DB_Reader()
 
 dateformat =  '%Y-%m-%d'
 newRecordCaptured = -1
 rowCount = 0   
+newTradeID = 0
+source_firm_group_name = 'JPMorgan'
 
 path = '/Users/lojinilogesparan/Documents/mifid_data/JPM/JPMorgan-ExecutionVenueReporting-RTS27-Q1-2018/'
 for foldername in os.listdir(path):
@@ -34,52 +38,30 @@ for foldername in os.listdir(path):
                     
                     with open(filenameEOD, 'rb') as csvfile:
                        data = csv.reader(csvfile, delimiter=',', quotechar='|')    
+                       
                        for row in data:
+                           
                            if len(row) >= 1:
                                if row[0] == 'Date of the trading day':
                                    rawdate = datetime.strptime(row[1],dateformat)
                            
                            if len(row) > 2:
-                               if row[1] == 'Financial Instrument': # start of data
-                                   
-                                   if newRecordCaptured > -1:
-                                       # Store previous record data
-                                       # ------------------------------
-                                       # Writing to Table 2
-                                       print table2_rec.getAttrArray()
-                                       #rtsdb.Write_to_Table2(table2_rec)
-                                       
-                                       # Writing to Table 4
-                                       table4_rec_new.FILE_ID = table2_rec.FILE_ID
-                                       table4_rec_new.INSTRUMENT_NAME = table2_rec.INSTRUMENT_NAME
-                                       table4_rec_new.ISIN = table2_rec.ISIN
-                                       table4_rec_new.CURRENCY = table2_rec.CURRENCY
-                                       print table4_rec_new.getAttrArray()
-                                       #rtsdb.Write_to_Table4(table4_rec_new)
-                                       
-                                       # Writing to Table 6 to Database
-                                       table6_rec_new.FILE_ID = table2_rec.FILE_ID
-                                       table6_rec_new.INSTRUMENT_NAME = table2_rec.INSTRUMENT_NAME
-                                       table6_rec_new.ISIN = table2_rec.ISIN
-                                       table6_rec_new.CURRENCY = table2_rec.CURRENCY
-                                       print table6_rec_new.getAttrArray()
-                                       #rtsdb.Write_to_Table6(table6_rec_new)
+                               if row[1] == 'Financial Instrument':
                                    
                                    # New record generation
                                    # ------------------------------
-                                   
                                    newRecordCaptured += 1
+                                   newTradeID += 1
+                                   
                                    # Building Table 2
                                    table2_rec = RTS27_Table_Records_Module.RTS27_Table2()   
                                    formatted_date = datetime.strftime(rawdate, "%Y-%m-%d")
-                                   table2_rec.setFileId(source_firm_name + "_" + str(0))
                                    table2_rec.setInstrumentName(str(row[2]))
                                    table2_rec.setISIN(str(row[3]))
                                    table2_rec.setSourceCompanyName(source_firm_name)
                                    table2_rec.setFileName(os.path.basename(filename))
                                    table2_rec.setTradeDate(formatted_date)
-                                   table2_rec.setFileId(source_firm_name)
-                               
+                                   
                                    # ------------------------------
                                    # Building Table 4
                                    table4_rec_new = RTS27_Table_Records_Module.RTS27_Table4()
@@ -94,9 +76,43 @@ for foldername in os.listdir(path):
                                    table6_rec_new.FILENAME = filename
                                    table6_rec_new.TRADE_DATE = formatted_date
                 
+                               # Table 1
+                               if row[0]=='Venue':
+                                   # Building Table 1 object
+                                   table1_rec = RTS27_Table_Records_Module.RTS27_Table1()
+                                   table1_rec.setFileName(os.path.basename(filename))
+                                   table1_rec.setSourceCompanyGroupName(source_firm_group_name)
+                                   table1_rec.setSourceCompanyName(str(row[1]))
+                                   table1_rec.setSourceCompanyCode(str(row[2]))
+                               if row[0] == 'Country of Competent Authority':
+                                   table1_rec.setCountryOfCompetentAuthority(str(row[1]))
+                               if row[0] == 'Market Segment':
+                                   table1_rec.setMarketSegmentName(row[1])
+                                   table1_rec.setMarketSegmentID(row[2])
+                                   if row[1] != '' or row[2] != '' or row[3] != '':
+                                       # CHECK DATA
+                                       print(row)
+                               if row[0] == 'Outages':
+                                   table1_rec.setOutagesNature(row[1])
+                                   table1_rec.setOutagesNumber(row[2])
+                                   table1_rec.setOutagesAverageDuration(row[3])
+                                   if row[1] != '' or row[2] != '' or row[3] != '':
+                                       # CHECK DATA
+                                       print(row)
+                               if row[0] == 'Scheduled Auction':
+                                   table1_rec.setScheduledAutionNature(row[1])
+                                   table1_rec.setScheduledAutionNumber(row[2])
+                                   table1_rec.setScheduledAutionAverageDuration(row[3])
+                                   if row[1] != '' or row[2] != '' or row[3] != '':
+                                       # CHECK DATA
+                                       print(row)
+                               if row[0] == 'Failed Transactions':
+                                   table1_rec.setFailedTransactionsNumber(row[2])
+                                   table1_rec.setFailedTransactionsPercent(row[3])
+                               
                                # Table 2
                                if row[1] == 'Instrument classification':
-                                   table2_rec.setInstrumentClassification(str(row[2]))
+                                   table2_rec.setInstrumentClassification(str(row[2]), rts_db_rd.getCfi_assetclass_map(), rts_db_rd.getCfi_char_map())
                                if row[1] == 'Currency':
                                    table2_rec.setCurrency(str(row[2]))
                                
@@ -158,6 +174,41 @@ for foldername in os.listdir(path):
                                        table6_rec_new.setNumberOfDesignatedMarketMaker(str(row[3]))
                                    else:
                                        table6_rec_new.setNumberOfDesignatedMarketMaker(str(' '))
+                                   
+                               if row[2] == 'Median time elapsed between request and provision of any corresponding quotes':
+                                   # Store previous record data
+                                   table2_rec.setFileId(table1_rec.SOURCE_COMPANY_CODE + "_" + formatted_date+ "_" + str(newTradeID))
+                           
+                                   # ------------------------------
+                                   # Writing to Table 1
+                                   table1_rec.setTradeDate(formatted_date)
+                                   table1_rec.ISIN = table2_rec.ISIN
+                                   table1_rec.INSTRUMENT_CLASSIFICATION = table2_rec.INSTRUMENT_CLASSIFICATION
+                                   table1_rec.INSTRUMENT_NAME = table2_rec.INSTRUMENT_NAME
+                                   table1_rec.CURRENCY = table2_rec.CURRENCY
+                                   table1_rec.FILE_ID = table2_rec.FILE_ID
+                                   #print table1_rec.getAttrArrayTable1()
+                                   rtsdb.Write_to_Table1(table1_rec)
+                                   
+                                   # Writing to Table 2
+                                   #print table2_rec.getAttrArray()
+                                   rtsdb.Write_to_Table2(table2_rec)
+                                   
+                                   # Writing to Table 4
+                                   table4_rec_new.FILE_ID = table2_rec.FILE_ID
+                                   table4_rec_new.INSTRUMENT_NAME = table2_rec.INSTRUMENT_NAME
+                                   table4_rec_new.ISIN = table2_rec.ISIN
+                                   table4_rec_new.CURRENCY = table2_rec.CURRENCY
+                                   #print table4_rec_new.getAttrArray()
+                                   rtsdb.Write_to_Table4(table4_rec_new)
+                                   
+                                   # Writing to Table 6 to Database
+                                   table6_rec_new.FILE_ID = table2_rec.FILE_ID
+                                   table6_rec_new.INSTRUMENT_NAME = table2_rec.INSTRUMENT_NAME
+                                   table6_rec_new.ISIN = table2_rec.ISIN
+                                   table6_rec_new.CURRENCY = table2_rec.CURRENCY
+                                   #print table6_rec_new.getAttrArray()
+                                   rtsdb.Write_to_Table6(table6_rec_new)
                                    
                            rowCount += 1                 
                            
--- Adapters/UBSRTS27Importer.py
+++ Adapters/UBSRTS27Importer.py
@@ -11,24 +11,31 @@ from datetime import datetime
 import RTS27_DB_Writer_Module
 import RTS27_Table_Records_Module
 rtsdb = RTS27_DB_Writer_Module.RTS27_DB_Writer()
+import RTS27_Prod_Class_DB_Reader_Module
+rts_db_rd = RTS27_Prod_Class_DB_Reader_Module.RTS27_Prod_Class_DB_Reader()
 
-path = "/Users/lojinilogesparan/Documents/mifid_data/ubs/ubs ag/Jan18/localstorage/dlprod/apps/be-engine/Jan2018"
+
+path = "/Users/lojinilogesparan/Documents/mifid_data/ubs/ubs ag/tmpJan"
 source_firm_name = "UBS AG London Branch"
+source_firm_group_name = 'UBS'
 dateformat =  '%Y-%m-%d' 
 
 fileCount = 0
+newTradeID = 0
 for filename in os.listdir(path):
     if len(filename) >= 12:
         if filename[:11].isalnum() and filename[12].isalnum()==False and filename.find('rfqId')==-1 and filename.find('quote')==-1:
             print(filename)
             fileCount += 1
             
+            table1start = 0
             table2start = 0
             table4start = 0
             table6start = 0
             with open(path+'/'+filename, 'rb') as inputfile:
                 
                 # Create tables
+                table1_rec = RTS27_Table_Records_Module.RTS27_Table1()
                 table2_rec = RTS27_Table_Records_Module.RTS27_Table2()
                 table4_rec_new = RTS27_Table_Records_Module.RTS27_Table4()
                 table6_rec_new = RTS27_Table_Records_Module.RTS27_Table6()
@@ -39,7 +46,12 @@ for filename in os.listdir(path):
                 
                 for line in inputfile:
                     # Determine start and end of relevant tables
-                    if line.find('table2')>0:
+                    if line.find('table1')>0:
+                        table1start += 1
+                        if table1start == 1:
+                            newTradeID +=1
+                        
+                    elif line.find('table2')>0:
                         table2start += 1
                             
                     elif line.find('table4')>0:    
@@ -50,6 +62,25 @@ for filename in os.listdir(path):
                             
 
                     # Start writing to arrays
+                    # ------------------------------
+                    # Building table 1
+                    if table1start == 1:
+                        if line.find('ExecutionVenueName')>0:
+                           table1_rec.setSourceCompanyName(line[line.find('>')+1:line.find('</')])
+                        if line.find('ExecutionVenueIdentifier')>0:
+                           table1_rec.setSourceCompanyCode(line[line.find('>')+1:line.find('</')])
+                        if line.find('CountryOfCompetentAuthority')>0:
+                           table1_rec.setCountryOfCompetentAuthority(line[line.find('>')+1:line.find('</')])
+                        if line.find('MarketSegmentName')>0:
+                           table1_rec.setMarketSegmentName(line[line.find('>')+1:line.find('</')])
+                        if line.find('MarketSegmentIdentifier')>0:
+                           table1_rec.setMarketSegmentID(line[line.find('>')+1:line.find('</')])
+                        if line.find('OutageNumber')>0:
+                           table1_rec.setOutagesNumber(line[line.find('>')+1:line.find('</')])
+                        if line.find('FailedTransactionsNumber')>0:
+                           table1_rec.setFailedTransactionsNumber(line[line.find('>')+1:line.find('</')])
+                    
+                    
                     if table2start == 1:
                         # ------------------------------
                         # Building Table 2
@@ -58,7 +89,10 @@ for filename in os.listdir(path):
                         if line.find('ISIN')>0:
                             table2_rec.setISIN(line[line.find('ISIN')+6:line.find('ISIN')+18])
                         if line.find('InstrumentClassification')> 0:
-                            table2_rec.setInstrumentClassification(line[line.find('"')+1:line.find('/')-1])
+                            if line[line.find('"')+1:line.find('/')-1] != 'CFI':
+                                table2_rec.setInstrumentClassification(line[line.find('"')+1:line.find('/')-1], rts_db_rd.getCfi_assetclass_map(), rts_db_rd.getCfi_char_map())
+                            else:
+                                table2_rec.setInstrumentClassification('', rts_db_rd.getCfi_assetclass_map(), rts_db_rd.getCfi_char_map())
                         if line.find('Currency')>0:
                             table2_rec.setCurrency(line[line.find('>')+1:line.find('/')-1])
                     
@@ -113,17 +147,30 @@ for filename in os.listdir(path):
                     
                     # When data gathering is complete
                     if line.find('</qoeReport>') >= 0:
-                        # -----------------------------------------------------------
                         # Complete Table 2
                         table2_rec.setFileId(source_firm_name + "_" + str(0)) 
                         table2_rec.setSourceCompanyName(source_firm_name)
                         table2_rec.setFileName(os.path.basename(filename))
                         table2_rec.setFileId(source_firm_name) 
                         table2_rec.setTradeDate(formatted_date)
+                        table2_rec.setFileId(table1_rec.SOURCE_COMPANY_CODE + "_" + formatted_date+ "_" + str(newTradeID))
                         
                         print table2_rec.getAttrArray()
                         # Writing to Table 2
                         rtsdb.Write_to_Table2(table2_rec)
+                        
+                        # -----------------------------------------------------------
+                        # Writing to Table 1
+                        table1_rec.setFileName(os.path.basename(filename))
+                        table1_rec.setSourceCompanyGroupName(source_firm_group_name)
+                        table1_rec.setTradeDate(formatted_date)
+                        table1_rec.ISIN = table2_rec.ISIN
+                        table1_rec.INSTRUMENT_CLASSIFICATION = table2_rec.INSTRUMENT_CLASSIFICATION
+                        table1_rec.INSTRUMENT_NAME = table2_rec.INSTRUMENT_NAME
+                        table1_rec.CURRENCY = table2_rec.CURRENCY
+                        table1_rec.FILE_ID = table2_rec.FILE_ID
+                        print table1_rec.getAttrArrayTable1()
+                        #rtsdb.Write_to_Table1(table1_rec)
 
                         # -----------------------------------------------------------
                         # Complete Table 4
@@ -135,9 +182,9 @@ for filename in os.listdir(path):
                         table4_rec_new.CURRENCY =  table2_rec.CURRENCY
                         table4_rec_new.TRADE_DATE = formatted_date
                         
-                        print table4_rec_new.getAttrArray()
+                        #print table4_rec_new.getAttrArray()
                         # Writing to Table 4
-                        rtsdb.Write_to_Table4(table4_rec_new)
+                        #rtsdb.Write_to_Table4(table4_rec_new)
                         
                         # -----------------------------------------------------------
                         # Complete Table 6
@@ -149,8 +196,8 @@ for filename in os.listdir(path):
                         table6_rec_new.ISIN = table2_rec.ISIN
                         table6_rec_new.CURRENCY = table2_rec.CURRENCY
                         
-                        print table6_rec_new.getAttrArray()
+                        #print table6_rec_new.getAttrArray()
                         # Writing to Table 6 to Database
-                        rtsdb.Write_to_Table6(table6_rec_new)
+                        #rtsdb.Write_to_Table6(table6_rec_new)
 
-rtsdb.__del__()
+#rtsdb.__del__()
