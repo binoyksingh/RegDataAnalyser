17de4b05d655027806a678b556f6f897a8b61dfb
Sarthak Agarwal <sarthak.agarwal212@gmail.com>
2019-07-21 17:11:48 +0100

    Add functions to getPriceFromTenor and getDaysInTenor

--- Modules/Curve_Module.py
+++ Modules/Curve_Module.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 
 import sys
+from decimal import Decimal
+import numpy as np
+from scipy import interpolate
+from scipy.interpolate import interp1d
 
 class Curve:
 
@@ -28,3 +32,29 @@ class Curve:
             mkt_data_detail_xml += "</CurvePoint>"
         mkt_data_detail_xml += "</"+ self.CurveType + ">"
         return mkt_data_detail_xml
+
+    def getPriceForTenor(self, tenor_days):
+        x_axis_points = []
+        y_axis_points = []
+        for curve_point in self.CurvePointList:
+            x_axis_points.append(self.getDaysInTenor(curve_point.TENOR) )
+            y_axis_points.append(Decimal(curve_point.MIDPRICE))
+
+        x2 = np.linspace(0, 360, 361)
+        # Currently the plots are only plotted to 361 days, so we need to ignore
+        # any trade with the tenor_days of over 361 days.
+        interp_val_linear = 0
+        if (int(tenor_days.days)<361):
+            if (len(x_axis_points)>1) :
+                 f_cubic = interp1d(x_axis_points, y_axis_points, kind='cubic')
+                 temp_cubic = f_cubic(x2)
+                 interp_val_linear = temp_cubic[int(tenor_days.days)]
+        return interp_val_linear
+
+    def getDaysInTenor(self, tenor):
+        tenor_days = 0
+        if "M" in tenor:
+            tenor_months = int(tenor.replace("M", ""))
+            tenor_days = int(tenor_months) * 30
+        return tenor_days
+
