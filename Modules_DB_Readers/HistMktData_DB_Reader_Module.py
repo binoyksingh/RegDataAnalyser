5bcd10b2a39c520bb35eefa8a91ada9ab58253d6
Sarthak Agarwal <sarthak.agarwal212@gmail.com>
2019-07-21 17:13:26 +0100

    New Functions to get FX Spot Rate and FX Fwd Rate

--- Modules_DB_Readers/HistMktData_DB_Reader_Module.py
+++ Modules_DB_Readers/HistMktData_DB_Reader_Module.py
@@ -5,7 +5,9 @@ import ConfigParser
 from collections import defaultdict
 import xml.etree.ElementTree as ET
 from Modules import CurvePoint_Module
+from Modules import Curve_Module
 import datetime
+import os
 
 class HistMktData_DB_Reader:
 
@@ -14,7 +16,8 @@ class HistMktData_DB_Reader:
 
     def __init__(self):
         print ('HistMktData_DB_Reader:INIT:Calling Constructor')
-        self.config.read("../Config/DatabaseConfig.txt")
+        #self.config.read("../Config/DatabaseConfig.txt")
+        self.config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../Config', 'DatabaseConfig.txt'))
 
         DB_HOSTNAME_POSTGRES = self.config.get("DATABASE_DETAILS", "DATABASE_HOST_POSTGRES")
         DB_USER_POSTGRES = self.config.get("DATABASE_DETAILS", "DATABASE_USER_POSTGRES")
@@ -26,10 +29,10 @@ class HistMktData_DB_Reader:
 
         self.cursor = self.connection.cursor()
 
-        print ('RTS27_DB_WRITER:INIT:Connection Success')
+        print ('HistMktData_DB_Reader:INIT:Connection Success')
 
     def __del__(self):
-        print ('RTS27_DB_WRITER:INIT:Calling destructor')
+        print ('HistMktData_DB_Reader:INIT:Calling destructor')
         self.connection.close()
 
     # Initialise the CFI Asset Class Map
@@ -70,3 +73,69 @@ class HistMktData_DB_Reader:
             histdata_map[x[0]] = [curve_point_list]
 
         return histdata_map
+
+    # get FX Forward rate closest to the trade execution time
+    def getFXFwdHistMktDataForCcy(self, currency_pair, trade_execution_timestamp, tenor_day):
+
+        select_FXHistMktDataString = "select price_ts, currency_pair, fx_spot_price_open_bid_quote, mkt_Data_detail" \
+                                     " from hist_market_data where price_ts in ( " \
+                                     " select max(price_ts) from hist_market_data where currency_pair=%s and " \
+                                      " price_ts < %s) and currency_pair=%s "
+
+        self.cursor.execute(select_FXHistMktDataString, (currency_pair,trade_execution_timestamp,currency_pair))
+
+        myresult =  self.cursor.fetchall()
+        curve = Curve_Module.Curve()
+        fx_mid_price_for_ccy_tenor = 0
+
+        for x in myresult:
+
+            mktDataDetailXMLStr = x[3]
+            spot_price = x[2]
+
+            # Put all this stuff into the curve class
+            cp_spot = CurvePoint_Module.Curve_Point()
+            cp_spot.setTenor("0M")
+            cp_spot.setMidPrice(spot_price)
+
+            # print mktDataDetailXMLStr
+            curve.addCurvePoint(cp_spot)
+
+            mktDataDetailXML = ET.fromstring(mktDataDetailXMLStr)
+            curvePointsXML = mktDataDetailXML.findall('CurvePoint')
+
+            for curve_point in curvePointsXML:
+                cp = CurvePoint_Module.Curve_Point()
+
+                for child in curve_point:
+                    if (child.tag == "Tenor"):
+                        cp.setTenor(child.text)
+
+                    if (child.tag == "MidPrice"):
+                        cp.setMidPrice(child.text)
+
+                curve.addCurvePoint(cp)
+
+            fx_mid_price_for_ccy_tenor = curve.getPriceForTenor(tenor_day)
+
+        return fx_mid_price_for_ccy_tenor
+
+    # get FX Spot rate closest to the trade execution time
+    def getFXSpotHistMktDataForCcyPair(self, currency_pair, trade_execution_timestamp):
+
+        select_FXHistMktDataString = "select price_ts, currency_pair, fx_spot_price_open_bid_quote, mkt_Data_detail" \
+                                     " from hist_market_data where price_ts in ( " \
+                                     " select max(price_ts) from hist_market_data where currency_pair=%s and " \
+                                     " price_ts < %s) and currency_pair=%s "
+
+        self.cursor.execute(select_FXHistMktDataString, (currency_pair, trade_execution_timestamp, currency_pair))
+
+        myresult = self.cursor.fetchall()
+        curve = Curve_Module.Curve()
+        fx_mid_price_for_ccy_tenor = 0
+
+        for x in myresult:
+
+            fx_spot_price_for_ccy = x[2]
+
+        return fx_spot_price_for_ccy
