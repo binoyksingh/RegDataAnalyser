0c65b6243ebd00147df2db6c3e70071c27c9b12c
Sarthak Agarwal <sarthak.agarwal212@gmail.com>
2020-02-17 22:48:17 +0000

    Refactor

---
+++ Modules/FILE_PROCESS_STATUS_Module.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import sys
+
+class File_Process_Status:
+
+    # Define list of attributes
+    FILE_NAME=""
+    FILE_TYPE=""
+    FILE_STATUS=""
+    START_TIME = ""
+    FINISHED_TIME = ""
+
+    def setFileName(self, FILE_NAME):
+        self.FILE_NAME = FILE_NAME
+
+    def setFileType(self, FILE_TYPE):
+        self.FILE_TYPE = FILE_TYPE
+
+    def setFileStatus(self, FILE_STATUS):
+        self.FILE_STATUS = FILE_STATUS
+
+    def setStartTime(self, START_TIME):
+        self.START_TIME = START_TIME
+
+    def setFinishedTime(self, FINISHED_TIME):
+        self.FINISHED_TIME = FINISHED_TIME
+
+    def getAttrArray(self):
+        single_record_array = [self.FILE_NAME,
+                               self.FILE_TYPE,
+                               self.FILE_STATUS,
+                               self.START_TIME,
+                               self.FINISHED_TIME
+                               ]
+        return single_record_array
